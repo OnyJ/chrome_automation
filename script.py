4a934ef55462bcec42c27f316681b8655456fce0
Josselin <julien.josselin75@hotmail.fr>
2024-03-10 18:42:06 +0100

    Edit: clean script.py

--- script.py
+++ script.py
@@ -3,24 +3,16 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 def main():
-    # Configuration des options de Chrome
     chrome_options = Options()
-    # Décommentez la ligne ci-dessous pour utiliser Chrome en mode sans tête
     # chrome_options.add_argument("--headless")
+    chromedriver_path = './chromedriver'
 
-    # Spécifiez le chemin vers chromedriver
-    chromedriver_path = './chromedriver'  # Ajustez le chemin selon votre configuration
     service = Service(executable_path=chromedriver_path)
 
-    # Initialisation du driver Chrome avec les options et le service spécifié
+    # Initialize Chrome driver with specified options & service
     driver = webdriver.Chrome(service=service, options=chrome_options)
     
-    # Naviguez vers une page
     driver.get('https://www.youtube.com')
-    
-    # Logique supplémentaire ici
-
-    # Fermez le navigateur
     driver.quit()
 
 if __name__ == '__main__':
